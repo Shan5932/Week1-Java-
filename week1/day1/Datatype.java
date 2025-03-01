38b81ff8943d8cdabf49668c1c1c2c7b1df2b19e
Shanmugam <89644909+Shan5932@users.noreply.github.com>
2025-03-01 22:37:40 +0530

    Add files via upload

---
+++ week1/day1/Datatype.java
@@ -0,0 +1,23 @@
+package week1.day1;
+
+public class Datatype {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		int rgno=9293;
+		char logo='S';
+		float enginenumber=2838383f;
+		double licensenumber=92.9382839293829;
+		long refnumber=8393839283L;
+		boolean iscar=true;
+	
+		System.out.println("the regno of the car is:"+rgno+" "+"name of the logo is:"+logo+" "+"name of the engine number:"+enginenumber+" "+"the no of license is:"+licensenumber);
+		
+		
+		
+
+	}
+
+}
---
+++ week1/day1/Firstprogram.java
@@ -0,0 +1,13 @@
+package week1.day1;
+
+public class Firstprogram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//syso ctrl space
+	System.out.println("Hello welcome to test leaf");
+	//shortcut for execution ctrl+fn+f11
+	}
+
+}
